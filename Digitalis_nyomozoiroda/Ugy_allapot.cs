ba06e5b143177b89e63c9e1ee0bd349bb440be6e
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-01-30 09:21:35 +0100

    Ugy_allapot osztaly a feladata nelkül

---
+++ Digitalis_nyomozoiroda/Ugy_allapot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozoiroda
+{
+    internal class Ugy_allapot
+    {
+        private string aktualis_allapot;
+
+        public Ugy_allapot(string aktualis_allapot)
+        {
+            this.aktualis_allapot = aktualis_allapot;
+        }
+
+        public string Aktualis_allapot { get => aktualis_allapot; set => aktualis_allapot = value; }
+
+        
+    }
+}
