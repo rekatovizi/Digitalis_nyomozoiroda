bcc94a680c964c3d190740d5c33b34e6d2060b00
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-01-30 08:59:24 +0100

    Felhasznalo osztaly kesz

---
+++ Digitalis_nyomozoiroda/Felhasznalo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozoiroda
+{
+    internal class Felhasznalo
+    {
+        private string nev;
+        private string azonosito;
+        private string szerepkor;
+
+        public Felhasznalo(string nev, string azonosito, string szerepkor)
+        {
+            this.nev = nev;
+            this.azonosito = azonosito;
+            this.szerepkor = szerepkor;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Azonosito { get => azonosito; set => azonosito = value; }
+        public string Szerepkor { get => szerepkor; set => szerepkor = value; }
+    }
+}
