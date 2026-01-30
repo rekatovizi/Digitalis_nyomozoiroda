addefcda82863c952c245039b90f67261d714b2d
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-01-30 09:59:04 +0100

    Idovonal_esemeny osztaly kesz

---
+++ Digitalis_nyomozoiroda/Idovonal_esemeny.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozoiroda
+{
+    internal class Idovonal_esemeny
+    {
+        private string datum;
+        private string esemeny_leiras;
+
+        public Idovonal_esemeny(string datum, string esemeny_leiras)
+        {
+            this.datum = datum;
+            this.esemeny_leiras = esemeny_leiras;
+        }
+
+        public string Datum { get => datum; set => datum = value; }
+        public string Esemeny_leiras { get => esemeny_leiras; set => esemeny_leiras = value; }
+    }
+}
