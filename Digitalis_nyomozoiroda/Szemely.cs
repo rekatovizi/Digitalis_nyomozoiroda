3f7784c2e540599fcc4a6bb3e839814bbf9f8e6c
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-01-30 09:05:35 +0100

    Szemely osztaly kesz

---
+++ Digitalis_nyomozoiroda/Szemely.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozoiroda
+{
+    internal class Szemely
+    {
+        private string nev;
+        private int eletkor;
+        private string megjegyzes;
+
+        public Szemely(string nev, int eletkor, string megjegyzes)
+        {
+            this.nev = nev;
+            this.eletkor = eletkor;
+            this.megjegyzes = megjegyzes;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Eletkor { get => eletkor; set => eletkor = value; }
+        public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
+    }
+}
