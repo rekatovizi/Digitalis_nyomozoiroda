0a0755104bb2007c0a73ca40158dd869da696a47
Réka Tóvizi <tovizi.reka@petrik.hu>
2026-01-30 08:46:09 +0100

    Add project files.

---
+++ Digitalis_nyomozoiroda/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Digitalis_nyomozoiroda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
